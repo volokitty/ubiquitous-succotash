444c8b12f1942d3b42d4bba9bf332adb7b213ad6
volokitty <v.volokitty@gmail.com>
2023-09-29 02:48:34 +0300

    feat: complete step_1_1

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Implement me!");
+#[derive(Default, Debug, Copy, Clone)]
+struct Point {
+    x: i32,
+    y: i32,
 }
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Point { x, y }
+    }
+}
+
+#[derive(Clone)]
+struct Polyline {
+    points: Vec<Point>,
+}
+
+impl Polyline {
+    fn new(points: Vec<Point>) -> Result<Self, String> {
+        if points.is_empty() {
+            return Err("Polyline must contain at least 1 point".to_string());
+        }
+
+        Ok(Polyline { points })
+    }
+}
+
+fn main() {}
